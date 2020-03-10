798cd72d01e18362d4b27dc00f7af4bdd112456d
CarlosOrduz <47404630+CarlosOrduz@users.noreply.github.com>
2020-03-09 20:25:01 -0500

    V2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,56 @@
 
 
 let socket = io.connect("http://localhost:3000", { forceNew: true });
+
 socket.on("participantes", data => {
-    //console.log(data);
+  
     render(data);
+    document.getElementById("registro").disabled= false ;
+   
+  });
+  socket.on("ofertas", data => {
+    render(data)
+    document.getElementById("oferta").disabled = true;
+    document.getElementById("registro").disabled= true ;
+    off=true;
+    setTimeout(function(){ data.forEach(e=>{
+      if(e.oferta.estado==="Oferta aceptada"){
+        document.getElementById("oferta").style.display = "none"; 
+        
+      }else{
+        document.getElementById("registro").disabled=false ;
+        document.getElementById("oferta").disabled=false ;
+        off=false;
+      }
+    })  }, 30000);
+    
   });
   function render(data) {
     let html = data
       .map((e, i) => {
+        if(e.oferta.estado==="Oferta rechazada"){
+          return `
+          <div>
+              <strong>${e.nombre}</strong>
+              <em>[${e.oferta.estado}. ]</em>
+          <div>
+          `;
+        }else if(e.oferta.estado==="Oferta aceptada"){
+          
+          
         return `
       <div>
           <strong>${e.nombre}</strong>
-          <em>[${e.oferta.estado}. Valor:${e.oferta.valor}]</em>
+          <em>[${e.oferta.estado}. Valor: $${e.oferta.valor}]</em>
       <div>
-      `;
+      `;}
+      
       })
       .join(" ");
-    console.log(html);
+    
     document.getElementById("ofertas").innerHTML = html;
-    document.getElementById("oferta").disabled = true;
-    document.getElementById("form1").style.display = "initial"; 
+   
+    
   
   }
 function register(){
@@ -37,5 +68,8 @@ function register(){
 }
 
 function ofertar(){
-    socket.emit("new-offer");  
+  
+    socket.emit("new-offer",document.getElementById("nit").value); 
+    
+    
 }
--- socketApi.js
+++ socketApi.js
@@ -2,57 +2,61 @@ let socket_io = require("socket.io");
 var io = socket_io();
 var socketApi = {};
 
-
 socketApi.io = io;
 
-let participantes = [
-  
-];
+let participantes = [];
 
 io.on("connection", function(socket) {
   io.sockets.emit("participantes", participantes);
 
   socket.on("new-participant", data => {
-     
     socketApi.sendNotification(data);
   });
-  socket.on("new-offer", () => {
-     
-    socketApi.updateOffer();
+  socket.on("new-offer", nit => {
+    socketApi.updateOffer(nit);
   });
 });
 
 socketApi.sendNotification = data => {
-
-
-    participantes.push(data);
-    console.log(participantes)
-    
-    
-
+  participantes.push(data);
+  
 };
-socketApi.updateOffer = () => {
-    if(participantes.length==1){
-        participantes[participantes.length-1].oferta.valor=150000000;
-    }else{
-        participantes[participantes.length-1].oferta.valor=participantes[participantes.length-2].oferta.valor+Math.floor(Math.random() * (10000000 - 5000000 +1) ) + 5000000;
-    }
-var pb=(Math.random() * (0.8 - 0.3) ) + 0.3;
-var po=(Math.random() * (0.8 - 0.3) ) + 0.3;
-console.log(po)
-console.log(pb)
-if(po>pb){
-    participantes[participantes.length-1].oferta.estado="Oferta aceptada";
-}  else{
-    participantes[participantes.length-1].oferta.estado="Oferta rechazada";
-}
-    console.log(participantes)
-      io.sockets.emit("participantes", participantes);
-    
+socketApi.updateOffer = nit => {
 
+    var mayor = 0;
+    var pb = Math.random() * (0.8 - 0.3) + 0.3;
+    var po = Math.random() * (0.8 - 0.3) + 0.3;
+    participantes.forEach(e => {
+      if (e.oferta.valor > mayor) {
+        mayor = e.oferta.valor;
+      }
+    });
+    participantes.forEach(e => {
+      if (e.nit === nit) {
+        
+        if (po > pb) {
+          e.oferta.estado = "Oferta aceptada";
+         
+        } else {
+          e.oferta.estado = "Oferta rechazada";
+        }
+        if(mayor==0){
+          e.oferta.valor=150000000
+        }else{
+          e.oferta.valor =
+          mayor +
+          Math.floor(Math.random() * (10000000 - 5000000 + 1)) +
+          5000000;
+        }
+      }
+      
+    });
+  
+
+  
+
+  io.sockets.emit("ofertas", participantes);
 };
-socketApi.showMessages = () => {
-  return db.collection("mensajes").find();
-};
+
 
 module.exports = socketApi;
